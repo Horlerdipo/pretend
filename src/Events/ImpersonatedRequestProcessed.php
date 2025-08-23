fc3d3310b4ec4620b5d65b69ce02635a509c40a3
Umar Oladipo <oladipoumar@gmail.com>
2025-08-23 09:46:17 +0100

    style:Pint formatting

--- src/Events/ImpersonatedRequestProcessed.php
+++ src/Events/ImpersonatedRequestProcessed.php
@@ -4,8 +4,8 @@ namespace Horlerdipo\Pretend\Events;
 
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Http\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Laravel\Sanctum\Contracts\HasAbilities;
+use Symfony\Component\HttpFoundation\Response;
 
 class ImpersonatedRequestProcessed
 {
